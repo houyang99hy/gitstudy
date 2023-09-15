637f3c91c54cc0a0cfca285c440973d76a9e8fac
houyang <1193197543@qq.com>
2023-09-15 15:11:44 +0800

    new files hello.java

---
+++ src/main/java/com/houyang/gitstudy/hello.java
@@ -0,0 +1,12 @@
+package com.houyang.gitstudy;
+
+/**
+ * @author houyang
+ * @description: TODO
+ * @date 2023/9/15 15:08
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
